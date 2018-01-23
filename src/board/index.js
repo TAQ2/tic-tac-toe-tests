1c3caf7705ce70289aeb972c9ad885c9167a1e50
Arjun <arjun.gupta@thepwgroup.co.uk>
2018-01-23 22:39:48 +0000

    add some line drawing combinations

--- src/board/index.js
+++ src/board/index.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 import Tile from "../tile";
+import { WINNING_LINE_OFFSET, BOARD_WIDTH, BOARD_HEIGHT } from "../constants";
+import { PRIMARY } from "../theme";
 
 const WinningLine = styled.line`
-  stroke: red;
+  stroke: ${PRIMARY};
   stroke-width: 10;
   stroke-dasharray: 2300;
   stroke-dashoffset: 2300;
+  stroke-linecap: round;
   animation: dash 1.5s linear forwards;
 
   @keyframes dash {
@@ -20,13 +23,10 @@ const WinningLine = styled.line`
 class Board extends Component {
   render() {
     const { onPlayerTurn, gameState } = this.props;
-    const width = 900;
-    const height = 900;
-
-    const lineOffset = 30;
-
+    const tileWidth = BOARD_WIDTH / 3;
+    const tileHeight = BOARD_HEIGHT / 3;
     return (
-      <svg width={width} height={height}>
+      <svg width={BOARD_WIDTH} height={BOARD_HEIGHT}>
         {gameState.map((row, rowIndex) =>
           row.map((tile, columnIndex) => (
             <Tile
@@ -36,16 +36,32 @@ class Board extends Component {
               columnIndex={columnIndex}
               token={tile}
               onTileClick={onPlayerTurn}
-              width={width}
-              height={height}
+              width={BOARD_WIDTH}
+              height={BOARD_HEIGHT}
             />
           ))
         )}
+        {/* if 00, 22 */}
+        {/*
         <WinningLine
-          x1={lineOffset}
-          y1={lineOffset}
-          x2={width - lineOffset}
-          y2={height - lineOffset}
+          x1={WINNING_LINE_OFFSET}
+          y1={WINNING_LINE_OFFSET}
+          x2={BOARD_WIDTH - WINNING_LINE_OFFSET}
+          y2={BOARD_HEIGHT - WINNING_LINE_OFFSET}
+        /> */}
+        {/* if 01, 21 */}
+        {/* <WinningLine
+          x1={tileWidth + tileWidth / 2}
+          y1={WINNING_LINE_OFFSET}
+          x2={tileWidth + tileWidth / 2}
+          y2={BOARD_HEIGHT - WINNING_LINE_OFFSET}
+        /> */}
+        {/* if 02, 20 */}
+        <WinningLine
+          x1={BOARD_WIDTH - WINNING_LINE_OFFSET}
+          y1={WINNING_LINE_OFFSET}
+          x2={WINNING_LINE_OFFSET}
+          y2={BOARD_HEIGHT - WINNING_LINE_OFFSET}
         />
       </svg>
     );
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
 export const PLAYER_X = "X";
 export const PLAYER_O = "O";
+
+export const BOARD_WIDTH = 900;
+export const BOARD_HEIGHT = 900;
+export const WINNING_LINE_OFFSET = 10;
