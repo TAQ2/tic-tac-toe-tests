144139e22ee2414c2e17df18eec02f78fa94fad2
Arjun <arjun.gupta@thepwgroup.co.uk>
2018-01-23 21:58:29 +0000

    a line and crap animation

--- src/board/index.js
+++ src/board/index.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 import Tile from "../tile";
+
+const WinningLine = styled.line`
+  stroke: red;
+  stroke-width: 10;
+  stroke-dasharray: 200;
+  stroke-dashoffset: 200;
+  animation: offset 2s linear forwards;
+
+  @keyframes offset {
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
+`;
 
 class Board extends Component {
   render() {
     const { onPlayerTurn, gameState } = this.props;
     const width = 600;
     const height = 600;
+
+    const lineOffset = 30;
 
     return (
       <svg width={width} height={height}>
@@ -24,6 +41,12 @@ class Board extends Component {
             />
           ))
         )}
+        <WinningLine
+          x1={lineOffset}
+          y1={lineOffset}
+          x2={width - lineOffset}
+          y2={height - lineOffset}
+        />
       </svg>
     );
   }
